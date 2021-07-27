67d7215f0cfa2da4083131ec62eb5d15775d4c2c
naziaq <79763949+naziaq@users.noreply.github.com>
2021-07-27 17:08:13 +0530

    MyDiarySpringCloudAPIGateway module of My Project API is commited.

---
+++ src/main/java/com/mydiary/api/gateway/MyDiarySpringCloudApiGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.mydiary.api.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyDiarySpringCloudApiGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyDiarySpringCloudApiGatewayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mydiary/api/gateway/MyDiarySpringCloudApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mydiary.api.gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyDiarySpringCloudApiGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
